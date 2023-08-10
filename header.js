85154e20b28ed92600ad8588af89abd203da69bd
nasuda <nasuda908@gmail.com>
2023-08-10 22:26:00 +0900

    first commit

---
+++ header.js
@@ -0,0 +1,6 @@
+const header = document.createElement('h1');
+
+let headerText = 'Header from header.js';
+header.innerText = headerText;
+
+export { header };
---
+++ index.js
@@ -0,0 +1,8 @@
+import { header } from './header.js';
+
+console.log('index.js');
+
+const root = document.getElementById('root');
+
+root.appendChild(header);
+
