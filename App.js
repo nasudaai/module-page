1e2fe34b108bbfd233745e37a74d5909e6be2e85
nasuda <nasuda908@gmail.com>
2023-08-10 23:48:43 +0900

    add App.js

---
+++ App.js
@@ -0,0 +1,8 @@
+import { header } from './header.js'
+import { btn } from './button.js'
+
+const page = document.createElement('div');
+page.appendChild(header);
+page.appendChild(btn);
+
+export default page;
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { header } from './header.js';
-import { btn } from './button.js';
+//import { header } from './header.js';
+//import { btn } from './button.js';
+import page from './App.js'
 
 console.log('index.js');
 
 const root = document.getElementById('root');
 
-root.appendChild(header);
-root.appendChild(btn);
+root.appendChild(page);
 
